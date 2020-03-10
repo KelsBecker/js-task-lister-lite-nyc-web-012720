2901844e92e5568c7282e0f6b9b88353b72a98cc
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-03-10 18:58:38 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let button = document.getElementsByTagName('input')[1]
+  
+  button.addEventListener('click', function(event){
+    event.preventDefault()
+    let listToUpdate = document.getElementById('tasks')
+    let text = document.querySelector('input').value
+
+    function userInput(text){
+      let li = document.createElement('li')
+      li.innerHTML = `${text} 
+      <button data-description ${text}>X</button>`
+      return li;
+    }
+    listToUpdate.appendChild(userInput(text))
+  })
 });
